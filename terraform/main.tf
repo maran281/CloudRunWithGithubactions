551c91e26158561423f96cf9b640086dd235be6e
mukesh sain <mukeshk.sain786@gmail.com>
2023-10-17 21:35:57 +0200

    c1

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "google_cloud_run_service" "my-second-cloud-run-service" {
     template {
         spec {
           containers {
-            image="us-east1-docker.pkg.dev/manojproject1-396309/githubaction-testing-cloudrun1/myimage1"
+            image="us-east1-docker.pkg.dev/manojproject1-396309/githubaction-testing-cloudrun1/githubactioncodeimage1"
           }
         }
     }
